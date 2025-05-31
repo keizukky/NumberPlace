8def867b8716254739859ce6d8ae1ec25d0a1895
keizukky <kei.zukky@gmail.com>
2025-06-01 08:50:14 +0900

    Initial Commit

---
+++ NumberPlace/NumberPlaceApp.swift
@@ -0,0 +1,15 @@
+//
+//  NumberPlaceApp.swift
+//  Copyright © 2025 keizukky. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct NumberPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
